9b272ee892698c64a220c80add6bff28ef548396
Travis Johnston <tjohnst1@gmail.com>
2017-07-03 14:14:54 -0700

    apply map styles; import normalizer

---
+++ src/components/map/mapConfig.js
@@ -0,0 +1,104 @@
+export const mapConfig = {
+  center: {
+    lat: 45.5223226,
+    lng: -122.6678265,
+  },
+  zoom: 12,
+  styles: [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "weight": "1.30"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#e0e0e0"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+  ],
+  mapTypeControl: false,
+  scaleControl: false,
+  streetViewControl: false,
+  zoomControl: false,
+};
+
+export const zoomConfig = {
+  increment: 1,
+}
