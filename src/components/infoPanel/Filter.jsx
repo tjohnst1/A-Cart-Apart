665fa13ea77482fc863dcb4b04b22caf489eca61
Travis Johnston <tjohnst1@gmail.com>
2017-07-05 22:11:30 -0700

    add basic filter component

---
+++ src/components/infoPanel/Filter.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { kebabCase } from 'lodash';
+import './filter.scss';
+
+const Filter = (props) => {
+  const { categories } = props;
+
+  const filterCheckboxes = categories.map((category, i) => {
+    const formattedName = kebabCase(category)
+    return (
+      <div className="filter__input-group" key={i}>
+        <input name={formattedName} type="checkbox"/> <label for={kebabCase(formattedName)}>{category}</label>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h3>Filters</h3>
+      {filterCheckboxes}
+    </div>
+  )
+}
+
+export default Filter;
