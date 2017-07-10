fb411ac90ad88a03d2cf61cce9717416cebc47d1
Travis Johnston <tjohnst1@gmail.com>
2017-07-09 17:22:04 -0700

    add menu bar

---
+++ src/components/menubar/MenuBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './menubar.scss';
+
+const MenuBar = (props) => {
+  return (
+    <div className="menu-bar">
+      <h1 className="menu-bar__logo">A Cart Apart</h1>
+    </div>
+  )
+}
+
+export default MenuBar;
