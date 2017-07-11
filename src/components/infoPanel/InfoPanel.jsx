4666e5a0814e0233fcd4f3337554e13ae48866f9
Travis Johnston <tjohnst1@gmail.com>
2017-07-10 22:42:20 -0700

    refactor front end

--- src/components/infoPanel/InfoPanel.jsx
+++ src/components/infoPanel/InfoPanel.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './infoPanel.scss';
 import CartInfo from './CartInfo';
+import FilterIcon from '../icons/FilterIcon';
+import Filter from '../filter/Filter';
 
 const InfoPanel = (props) => {
   const { currentCart, handleToggleFilter } = props;
 
   return (
     <div className="info-panel">
+      <div className="info-panel__headline">
+        <h1>A Cart Apart</h1>
+        <button onClick={() => handleToggleFilter()}><FilterIcon /><span>Filter</span></button>
+      </div>
+      <Filter />
       <CartInfo currentCart={currentCart}/>
     </div>
   );
--- src/containers/Index.jsx
+++ src/containers/Index.jsx
@@ -3,8 +3,6 @@ import { connect } from 'react-redux';
 import Map from '../components/map/Map';
 import { getCartDataIfNeeded, showCartInfo } from '../actions/carts'
 import { storeMapReference, initializeMarkers, toggleFilter } from '../actions/map'
-import SideBar from '../components/sideBar/SideBar'
-import MenuBar from '../components/menubar/MenuBar'
 import InfoPanel from '../components/infoPanel/InfoPanel'
 
 class Index extends Component {
@@ -16,14 +14,13 @@ class Index extends Component {
     const { cartData, currentCart, categories } = this.props.cartData;
     const { mapReference, markers, showFilter } = this.props.map;
     const { handleShowCartInfo, handleStoreMapReference, handleInitializeMarkers, handleToggleFilter } = this.props;
+    // <SideBar handleToggleFilter={handleToggleFilter} showFilter={showFilter} currentCart={currentCart}/>
 
     if (cartData) {
       return (
         <div style={{width: '100%', height: '100%'}}>
-          <MenuBar />
           <main>
-            <InfoPanel currentCart={currentCart} />
-            <SideBar handleToggleFilter={handleToggleFilter} showFilter={showFilter} currentCart={currentCart}/>
+            <InfoPanel currentCart={currentCart} handleToggleFilter={handleToggleFilter}/>
             <Map mapReference={mapReference} cartData={cartData} currentCart={currentCart} handleShowCartInfo={handleShowCartInfo} categories={categories}
               handleStoreMapReference={handleStoreMapReference} handleInitializeMarkers={handleInitializeMarkers} markerData={markers} showFilter={showFilter} handleToggleFilter={handleToggleFilter} />
           </main>
